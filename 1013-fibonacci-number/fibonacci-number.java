2444e7a0be94c088fb5a951790b0c694ae96b42c
Yasoob Rashid <yasoobjavaid080@gmail.com>
2025-10-10 01:13:12 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (81.99%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,8 +1,22 @@
 class Solution {
+    int[] dp;
     public int fib(int n) {
+        
+        dp = new int[n+1];
+        Arrays.fill(dp,-1);
+
+        return helper(n);   
+    }
+
+    public int helper(int n){
         if(n==0||n==1){
             return n;
         }
-        return fib(n-1) + fib(n-2);
+
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        dp[n] = helper(n-1) + helper(n-2);
+        return dp[n];
     }
 }
